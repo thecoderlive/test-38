aadd3e1e960feaf019ae1e6fa44a240b6d7bbddf
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 02:10:44 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1cdbf4c6-b730-4daf-8b75-90ad888e399d",
+    "home_feed": [
+        {
+            "id": "27ff9f6c-d1ef-427d-9c0e-2c6476e2c951",
+            "artist_name": "course",
+            "home_carousel": [
+                {
+                    "id": "7e2747e4-5403-4008-bbfe-9dbfc6c4fbb5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "ed949e1f-787b-4409-ba92-1139c3531b2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "d9751444-3d91-4619-b417-76c0fdc231d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "8ea7826f-9abd-4857-95ac-6e2fb5e34319",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "fbbc95c0-ba53-4813-91fb-12f4f10e86e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                }
+            ],
+            "art_caption": "positive",
+            "art_description": "Most agency face around rather end soon. Before president conference suffer. Thought address explain case hair."
+        },
+        {
+            "id": "9af778c0-5937-4e51-88fc-36376d4c3c22",
+            "artist_name": "ground",
+            "home_carousel": [
+                {
+                    "id": "06f2dd84-b740-49d5-ab5c-9af90ea218ad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "e80f4685-ae64-473c-a5b5-e3e98446f37e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "56cfdec9-8f9f-4833-92e0-6d5a2312bb9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "a0e73b1d-0969-4060-bb8c-c46ea0f1016a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "2f4ddcc2-40c2-4177-943d-40f24536bda3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                }
+            ],
+            "art_caption": "listen",
+            "art_description": "Help me continue tend. Company weight century political. Wrong five bar finally sense high question."
+        },
+        {
+            "id": "0af1e85a-3213-47d9-9c5c-1dc949fa0c6f",
+            "artist_name": "fund",
+            "home_carousel": [
+                {
+                    "id": "cef5b5de-1bb6-4c4b-b25d-ed57edc3694d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "51c79201-8997-49f7-ab17-32db9e9a4645",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "90508659-e1f3-4126-be17-dbba6767c056",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                },
+                {
+                    "id": "1cb1a029-d985-4354-b103-0779b7b81f2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                },
+                {
+                    "id": "fdc0d2b0-a1c9-4f21-ad83-3bb472fa1443",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                }
+            ],
+            "art_caption": "although",
+            "art_description": "Foot age boy choose realize into fly."
+        },
+        {
+            "id": "2c935ac5-1a1a-42fb-862c-414ec6d60dad",
+            "artist_name": "my",
+            "home_carousel": [
+                {
+                    "id": "962fc915-2deb-4369-9310-ef878e8848c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                },
+                {
+                    "id": "eb40d0df-7798-4814-a927-c0d880732e4b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                },
+                {
+                    "id": "b838e86c-7b2f-4746-a71e-a776d07948ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "0c156e39-1105-486c-8c67-afe901e633a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png"
+                },
+                {
+                    "id": "fcce63af-f619-435c-8933-72e68a924280",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                }
+            ],
+            "art_caption": "space",
+            "art_description": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article."
+        },
+        {
+            "id": "37f6d8e0-0816-424e-ac8c-930919742021",
+            "artist_name": "general",
+            "home_carousel": [
+                {
+                    "id": "afea1671-b99d-4048-96d7-263444ef2cbe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "28a4defd-97d5-4d2e-94b9-f0f589d59463",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "66b9a7e2-22c8-48ca-8edf-f95531a5b960",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "695c1dcb-8a00-4db3-83ed-bbf9e0315cb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "f716f4a1-2ebf-4d96-8acc-630f9768623f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                }
+            ],
+            "art_caption": "west",
+            "art_description": "Structure head win mean gun site peace. Floor act eat hit. Treat keep region yes artist mission."
+        }
+    ]
+}
